291429415275c6718871399ce806c287427ebe8c
Pavel Smolov <plexsmol@gmail.com>
2023-04-20 20:34:56 -0400

    feat: create first service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller('/api')
 export class AppController {
+  constructor(private appService: AppService) { }
+
   @Get('/users')
   getUsers() {
-    return [{ id: 1, name: 'Leni Archer' }];
+    return this.appService.getUsers();
   }
 }
