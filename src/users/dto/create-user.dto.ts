981c3ba8dfcd12cc656be95fac57f74b69b76cb6
Pavel Smolov <plexsmol@gmail.com>
2023-04-22 12:50:05 -0400

    feat: swagger document users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
+  @ApiProperty({ example: 'user@email.com', description: 'Users email' })
   readonly email: string;
+  @ApiProperty({ example: '1234', description: 'Password' })
   readonly password: string;
 }
