2a262b57802b2927afd637467bc6592361e7f532
Pavel Smolov <plexsmol@gmail.com>
2023-04-24 20:38:16 -0400

    feat: add pipes, validation pipe

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, Length, IsEmail } from 'class-validator';
 export class CreateUserDto {
   @ApiProperty({ example: 'user@email.com', description: 'Users email' })
+  @IsString({ message: 'Must be a string' })
+  @IsEmail({}, { message: 'Invalid email' })
   readonly email: string;
+
   @ApiProperty({ example: '1234', description: 'Password' })
+  @IsString({ message: 'Must be a string' })
+  @Length(4, 16, { message: 'Length should be min 4 and max 16' })
   readonly password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -8,6 +15,7 @@ import { Roles } from 'src/auth/roles-auth.decorator';
 import { RolesGuard } from 'src/auth/roles-guard';
 import { AddRoleDto } from './dto/add-role.dto';
 import { BanUserDto } from './dto/ban-user.dto';
+import { ValidationPipe } from 'src/pipes/validation-pipe';
 
 @ApiTags('Users')
 @Controller('users')
@@ -16,6 +24,7 @@ export class UsersController {
 
   @ApiOperation({ summary: 'User creation' })
   @ApiResponse({ status: 200, type: User })
+  @UsePipes(ValidationPipe)
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
